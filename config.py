641fc19d8c1e093cb5e38758dab1217f4229246c
kvvv1 <kaikevns1@gmail.com>
2024-05-30 20:54:22 -0300

    teste

--- config.py
+++ config.py
@@ -1,20 +1,25 @@
 from dotenv import load_dotenv
 import os
 
+# Especificar o caminho do arquivo key.env
+load_dotenv('key.env')
+
 class Config:
     # Configurações básicas do Flask
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     UPLOAD_FOLDER = 'static/uploads'
-
     
     # Configurações do SendGrid
-    load_dotenv()
     SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
     SENDER_EMAIL = os.environ.get('SENDER_EMAIL') or 'kaikevns1@gmail.com'
     
     # CELERY CONFIGURATION
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+    # Depuração para verificar se a chave foi carregada
+    print(f'SENDGRID_API_KEY: {SENDGRID_API_KEY}')  # Depuração
+
     
     
   #  class Config:
